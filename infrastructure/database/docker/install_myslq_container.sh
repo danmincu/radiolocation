ea83d847e74de9c3051b04e7cd3ae82d7c77116e
Dan Mincu <danmincu@gmail.com>
2018-11-05 18:30:10 -0500

    parameterized the creation of mysql initial database and user; fix a bug with container up detection.

--- infrastructure/database/docker/install_myslq_container.sh
+++ infrastructure/database/docker/install_myslq_container.sh
@@ -12,19 +12,22 @@ systemctl start docker
 
 MYSQL_CONTAINER="mysql"
 MYSQL_PORT=3306
+MYSQL_INIT_DATABASE="security"
 MYSQL_ROOT_PASSWORD="jordan2006"
+MYSQL_USER="user"
+MYSQL_USER_PASSWORD="jordan2006"
 
 echo "1. Creates a mySQL container after downloading the image"
-docker run --name=$MYSQL_CONTAINER -d -p $MYSQL_PORT:3306 -t -e MYSQL_DATABASE='security' -e MYSQL_USER='user' -e MYSQL_PASSWORD='jordan2006' mysql/mysql-server
+docker run --name=$MYSQL_CONTAINER -d -p $MYSQL_PORT:3306 -t -e MYSQL_DATABASE=$MYSQL_INIT_DATABASE -e MYSQL_USER=$MYSQL_USER -e MYSQL_PASSWORD=$MYSQL_USER_PASSWORD mysql/mysql-server
 
 echo "2. Waits for the $MYSQL_CONTAINER container be up and running"
 
 while true ; do 
-  result=$(docker container ls | grep $MYSQL_CONTAINER| grep healthy)
+  result=$(docker container ls | grep $MYSQL_CONTAINER| grep healthy| tr ' ' '.'| xargs)
   if [ -z $result ]; then
     printf '.'
   else
-    echo "Container is up!"
+    echo "$MYSQL_CONTAINER container is up!"
     break
   fi
   sleep 3
@@ -37,5 +40,8 @@ echo "root password is $password"
 echo "reset root password to $MYSQL_ROOT_PASSWORD"
 docker exec -i mysql mysql -uroot --connect-expired-password --password=$password <<< "ALTER USER 'root'@'localhost' IDENTIFIED BY '$MYSQL_ROOT_PASSWORD';"
 
-# USE THIS ONLY IF YOU WANT TO ENTER BASH
+# USE THIS ONLY IF YOU WANT TO ENTER BASH AS ROOT
 docker exec -it mysql mysql -uroot --password=$MYSQL_ROOT_PASSWORD
+
+# USE THIS ONLY IF YOU WANT TO ENTER BASH AS $MYSQL_USER
+docker exec -it mysql mysql -u$MYSQL_USER --password=$MYSQL_USER_PASSWORD
