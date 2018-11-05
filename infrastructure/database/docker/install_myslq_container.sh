14915de8908b21d7c3774fb960234aab3289ebff
Dan Mincu <danmincu@gmail.com>
2018-11-05 10:06:46 -0500

    Fix the mysql change of the root password

--- infrastructure/database/docker/install_myslq_container.sh
+++ infrastructure/database/docker/install_myslq_container.sh
@@ -12,7 +12,7 @@ systemctl start docker
 
 MYSQL_CONTAINER="mysql"
 MYSQL_PORT=3306
-
+MYSQL_ROOT_PASSWORD="jordan2006"
 
 echo "1. Creates a mySQL container after downloading the image"
 docker run --name=$MYSQL_CONTAINER -d -p $MYSQL_PORT:3306 -t -e MYSQL_DATABASE='cellsites' -e MYSQL_USER='user' -e MYSQL_PASSWORD='jordan2006' mysql/mysql-server
@@ -34,10 +34,8 @@ done
 password=$(docker logs $MYSQL_CONTAINER 2>&1 | grep GENERATED | tr '\r' ' '| cut -d ":" -f2 | xargs)
 
 echo "root password is $password"
+echo "reset root password to $MYSQL_ROOT_PASSWORD"
+docker exec -i mysql mysql -uroot --connect-expired-password --password=$password <<< "ALTER USER 'root'@'localhost' IDENTIFIED BY '$MYSQL_ROOT_PASSWORD';"
 
-docker exec -it $MYSQL_CONTAINER mysql -uroot --password=$password 
-
-#<<< "ALTER USER 'root'@'localhost' IDENTIFIED BY 'jordan2006';"
-
-#docker exec -it $MYSQL_CONTAINER mysql -uroot --password='AJBYP)udeg%Yp0hx0B.EKRYwAbO'
-#ALTER USER 'root'@'localhost' IDENTIFIED BY 'jordan2006';
+# USE THIS ONLY IF YOU WANT TO ENTER BASH
+docker exec -it mysql mysql -uroot --password=$MYSQL_ROOT_PASSWORD
