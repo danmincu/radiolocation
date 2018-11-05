d933a35b28ab04a028afa444a0d6f646a16af250
Dan Mincu <danmincu@gmail.com>
2018-11-05 02:49:28 -0500

    checkpoint for mysql container

--- infrastructure/database/docker/install_myslq_container.sh
+++ infrastructure/database/docker/install_myslq_container.sh
@@ -15,10 +15,29 @@ MYSQL_PORT=3306
 
 
 echo "1. Creates a mySQL container after downloading the image"
-docker run --name=$MYSQL_CONTAINER -d -p $MYSQL_PORT:3306 -t mysql/mysql-server
+docker run --name=$MYSQL_CONTAINER -d -p $MYSQL_PORT:3306 -t -e MYSQL_DATABASE='cellsites' -e MYSQL_USER='user' -e MYSQL_PASSWORD='jordan2006' mysql/mysql-server
 
 echo "2. Waits for the $MYSQL_CONTAINER container be up and running"
-until $(docker container ls | grep $MYSQL_CONTAINER| grep healthy); do
+
+while true ; do 
+  result=$(docker container ls | grep $MYSQL_CONTAINER| grep healthy)
+  if [ -z $result ]; then
     printf '.'
-    sleep 5
+  else
+    echo "Container is up!"
+    break
+  fi
+  sleep 3
 done
+
+
+password=$(docker logs $MYSQL_CONTAINER 2>&1 | grep GENERATED | tr '\r' ' '| cut -d ":" -f2 | xargs)
+
+echo "root password is $password"
+
+docker exec -it $MYSQL_CONTAINER mysql -uroot --password=$password 
+
+#<<< "ALTER USER 'root'@'localhost' IDENTIFIED BY 'jordan2006';"
+
+#docker exec -it $MYSQL_CONTAINER mysql -uroot --password='AJBYP)udeg%Yp0hx0B.EKRYwAbO'
+#ALTER USER 'root'@'localhost' IDENTIFIED BY 'jordan2006';
